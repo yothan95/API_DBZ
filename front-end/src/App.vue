ea4f25dac4d57066d1c9182336df2b8b11efc6a5
yothan95 <nathantoboul@hotmail.fr>
2022-06-15 01:25:37 +0200

    'front-end'

---
+++ front-end/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo_dbz.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="css">
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+
+.titre {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  font-size: 40px;
+  font-weight: bold;
+}
+
+.p1 {
+  color: yellow;
+  -webkit-text-stroke: 1px black;
+  font-family: arial;
+}
+
+.p2 {
+  color: red;
+  -webkit-text-stroke: 1px black;
+  font-family: arial;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.p3 {
+  color: black;
+  font-family: arial;
+  font-size: 25px;
+  letter-spacing: 0.3em;
+  margin-bottom: 100px;
+  margin-top: -30px;
+}
+
+.p4 {
+  display: flex;
+  justify-content: left;
+  font-family: arial;
+}
+
+img {
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  height: 200px;
+  width: 200px;
+  margin-bottom: -35px;
+}
+</style>
---
+++ front-end/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="titre">
+      <p class="p1">Dragon</p><p class="p2">Ball Z</p>
+    </div>
+
+    <p class="p3">Characters</p>
+    
+    <div class="hello">
+    <div :key="index" v-for="(characters, index) in characters">
+    <table>
+        <p class="p4">Nom : {{ characters.nom }}</p>
+        <p class="p4">Race : {{ characters.race }}</p>
+        <p class="p4">Description : {{ characters.description }}</p>
+        <p class="p4">Technique : {{ characters.technique }}</p>
+        <img v-bind:src="characters.image">
+        
+        <br>
+    </table>
+    </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      characters: null,
+    }
+  },
+  mounted(){
+    axios
+    .get("http://localhost:8082/personnages")
+    .then((reponse) => { 
+    this.characters = reponse.data;
+    console.log(this.characters)
+    });
+  },
+}
+</script>
+
+<style scoped lang="css">
+
+</style>
